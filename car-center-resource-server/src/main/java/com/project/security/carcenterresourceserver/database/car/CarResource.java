f785d43d821b345171e11434896523851fa87698
Dominika <dominikadolik6@gmail.com>
2019-06-04 19:21:04 +0200

    update carresource.java

--- car-center-resource-server/src/main/java/com/project/security/carcenterresourceserver/database/car/CarResource.java
+++ car-center-resource-server/src/main/java/com/project/security/carcenterresourceserver/database/car/CarResource.java
@@ -2,10 +2,9 @@ package com.project.security.carcenterresourceserver.database.car;
 
 import com.project.security.carcenterresourceserver.database.repositories.CarsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rest/car")
@@ -18,6 +17,11 @@ public class CarResource {
     public String add(@RequestBody final Car car) {
         carsRepository.save(car);
         return "added";
+    }
+
+    @GetMapping(path = "/getCars")
+    public List<Car> get() {
+        return carsRepository.findAll();
     }
 
 }
